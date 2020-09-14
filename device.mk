8ba78ab044e80741d6e8b20160d60fda958e5b7a
“abhijit” <lordhacker348@gmail.com>
2020-09-15 02:17:03 +0530

    Added Gcam Go

--- device.mk
+++ device.mk
@@ -56,6 +56,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     GParts
 
+# GCam Go
+PRODUCT_PACKAGES += \
+    GCam
+    
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
